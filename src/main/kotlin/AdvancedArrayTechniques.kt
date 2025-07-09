a55238f2db180bc5379c2139765b26ba8b057079
satyampandey30052025 <recro.satyam@bigbasket.com>
2025-07-09 12:20:16 +0530

    [1.2.3] Valid Sudoku

--- src/main/kotlin/AdvancedArrayTechniques.kt
+++ src/main/kotlin/AdvancedArrayTechniques.kt
@@ -6,34 +6,56 @@ class AdvancedArrayTechniques {
         val prefArray = IntArray(nums.size)
         prefArray[0] = 1
         val suffArray = IntArray(nums.size)
-        suffArray[nums.size-1]=1
-        for (i in 1..nums.size-1) {
-            prefArray[i] = prefArray[i - 1] * nums[i-1]
+        suffArray[nums.size - 1] = 1
+        for (i in 1..nums.size - 1) {
+            prefArray[i] = prefArray[i - 1] * nums[i - 1]
         }
-        for(i in nums.size-2 downTo 0){
-            suffArray[i] = suffArray[i+1] * nums[i+1]
+        for (i in nums.size - 2 downTo 0) {
+            suffArray[i] = suffArray[i + 1] * nums[i + 1]
         }
         val retArray = IntArray(nums.size)
-        for(i in retArray.indices){
+        for (i in retArray.indices) {
             retArray[i] = prefArray[i] * suffArray[i]
         }
         return retArray
     }
 
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
-      val freqMap = mutableMapOf<Int,Int>()
-      for(num in nums){
-          freqMap[num] = freqMap.getOrDefault(num,0)+1
-      }
-      val pq = PriorityQueue<Map.Entry<Int,Int>>(compareByDescending { it.value })
-      for(entry in freqMap){
-          pq.add(entry)
-      }
-      val retArray = IntArray(k)
-      for(i in retArray.indices){
-          retArray[i] = pq.poll().key
-      }
+        val freqMap = mutableMapOf<Int, Int>()
+        for (num in nums) {
+            freqMap[num] = freqMap.getOrDefault(num, 0) + 1
+        }
+        val pq = PriorityQueue<Map.Entry<Int, Int>>(compareByDescending { it.value })
+        for (entry in freqMap) {
+            pq.add(entry)
+        }
+        val retArray = IntArray(k)
+        for (i in retArray.indices) {
+            retArray[i] = pq.poll().key
+        }
         return retArray
+    }
+
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rowSet = Array(9) { mutableSetOf<Char>() }
+        val colSet = Array(9) { mutableSetOf<Char>() }
+        val boxSet = Array(9) { mutableSetOf<Char>() }
+        for (i in board.indices) {
+            for (j in board[0].indices) {
+                val char = board[i][j]
+                if(board[i][j]=='.'){
+                    continue
+                }
+                val subBoxIndex = (i / 3 * 3) + j / 3
+                if (rowSet[i].contains(char) || colSet[j].contains(char) || boxSet[subBoxIndex].contains(char)) {
+                    return false
+                }
+                rowSet[i].add(char)
+                colSet[j].add(char)
+                boxSet[subBoxIndex].add(char)
+            }
+        }
+        return true
     }
 
 }
