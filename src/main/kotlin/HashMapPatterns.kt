133a943f446cf79bc3e34ef29184e613579516b6
satyampandey30052025 <recro.satyam@bigbasket.com>
2025-07-22 12:35:59 +0530

    [1.3.3] 4Sum2

--- src/main/kotlin/HashMapPatterns.kt
+++ src/main/kotlin/HashMapPatterns.kt
@@ -8,11 +8,46 @@ class HashMapPatterns {
         var retValue = 0
         for (i in nums.indices) {
             for (j in i until nums.size) {
-                if (prefixSumArr[j+1] - prefixSumArr[i+1] == k) {
+                if (prefixSumArr[j + 1] - prefixSumArr[i + 1] == k) {
                     retValue++
                 }
             }
         }
         return retValue
+    }
+
+    fun subarraySumO(nums: IntArray, k: Int): Int {
+        var retValue = 0
+        var prefixSumFreq = mutableMapOf<Int, Int>()
+        prefixSumFreq[0] = 1
+        var currSum = 0
+        for (num in nums) {
+            currSum += num
+            val complement = currSum - k
+            if (prefixSumFreq.contains(complement)) {
+                retValue += prefixSumFreq[complement]!!
+            }
+            prefixSumFreq[currSum] = prefixSumFreq.getOrDefault(currSum, 0) + 1
+        }
+        return retValue
+    }
+
+    fun fourSumCount(nums1: IntArray, nums2: IntArray, nums3: IntArray, nums4: IntArray): Int {
+        val sumMap = mutableMapOf<Int, Int>()
+        for (a in nums1) {
+            for (b in nums2) {
+                val sum = a + b
+                sumMap[sum] = sumMap.getOrDefault(sum, 0) + 1
+            }
+        }
+        1
+        var count = 0
+        for (a in nums3) {
+            for (b in nums4) {
+                val target = -(a + b)
+                count += sumMap.getOrDefault(target, 0) + 1
+            }
+        }
+        return count
     }
 }
