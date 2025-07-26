27c26f47a2716993eb5123155d74b0ff219461e1
Satyam <satyampandey.learn211@gmail.com>
2025-07-27 00:41:11 +0530

    [2.2.3] Trapping rain water solved

--- src/main/kotlin/AdvancedTwoPointers.kt
+++ src/main/kotlin/AdvancedTwoPointers.kt
@@ -18,4 +18,23 @@ class AdvancedTwoPointers {
         }
         return retValue
     }
+
+    fun trap(height: IntArray): Int {
+        val lMax = IntArray(height.size)
+        val rMax = IntArray(height.size)
+        lMax[0] = height[0]
+        rMax[height.size - 1] = height[height.size-1]
+        for (i in 1..height.size - 1) {
+            lMax[i] = max(height[i], lMax[i - 1])
+        }
+        for (i in height.size - 2 downTo 0) {
+            rMax[i] = max(height[i], rMax[i + 1])
+        }
+        var retValue = 0
+        for (i in height.indices) {
+            val trappedWater = min(lMax[i], rMax[i]) - height[i]
+            retValue += trappedWater
+        }
+        return retValue
+    }
 }
