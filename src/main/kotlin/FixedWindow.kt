ae84b4fc46d7a04e43275bf5796e6240e299f940
Satyam <satyampandey.learn211@gmail.com>
2025-07-27 13:59:31 +0530

    [3.1.2] Permutation in a string

--- src/main/kotlin/FixedWindow.kt
+++ src/main/kotlin/FixedWindow.kt
@@ -11,14 +11,45 @@ class FixedWindow {
             currAvg = currSum / (i + 1)
         }
         var retVal = currAvg
-        while (windowE < nums.size-1) {
-              currSum-=nums[windowS]
-              windowS++
-              windowE++
-              currSum+=nums[windowE]
-              currAvg = currSum/k
-              retVal = max(currAvg,retVal)
+        while (windowE < nums.size - 1) {
+            currSum -= nums[windowS]
+            windowS++
+            windowE++
+            currSum += nums[windowE]
+            currAvg = currSum / k
+            retVal = max(currAvg, retVal)
         }
         return retVal
+    }
+
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        if(s2.length < s1.length) return false
+        val freqArrS1 = IntArray(26)
+        for (s in s1) {
+            freqArrS1[s - 'a'] += 1
+        }
+        var windowS = 0
+        var windowE = s1.length - 1
+        val sb = StringBuilder()
+        for (i in 0..windowE) {
+            sb.append(s2[i])
+        }
+        val freqArrS2 = IntArray(26)
+        for (s in sb) {
+            freqArrS2[s - 'a'] += 1
+        }
+        if (freqArrS1.contentEquals(freqArrS2)) {
+            return true
+        }
+        while (windowE < s2.length - 1) {
+            freqArrS2[s2[windowS] - 'a'] -= 1
+            windowS++
+            windowE++
+            freqArrS2[s2[windowE] - 'a'] += 1
+            if (freqArrS1.contentEquals(freqArrS2)) {
+                return true
+            }
+        }
+        return false
     }
 }
